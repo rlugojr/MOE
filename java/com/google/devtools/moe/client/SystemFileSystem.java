2fd2a21004aeba5bb2179e84a40f01cef1da59d8
cgruber <cgruber@google.com>
2015-12-03 16:23:47 -0800

    Make SystemFileSystem do the right thing for delete recursively on Java7.
    	Change on 2015/12/03 by cgruber <cgruber@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=109359602

--- java/com/google/devtools/moe/client/SystemFileSystem.java
+++ java/com/google/devtools/moe/client/SystemFileSystem.java
@@ -31,7 +31,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
